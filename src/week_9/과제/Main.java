3e322eb018aff1d10c23ef540770a575e1a2627b
ksg04 <ksg04001@hanmail.net>
2023-05-05 17:47:08 +0900

    10주차 과제1번 사칙연산 연산자 1개만 하자... 힘들다 .. < 완료 >

--- src/week_9/과제/Main.java
+++ src/week_9/과제/Main.java
@@ -3,24 +3,23 @@ package week_9.과제;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 interface Calculator {
 
     String returnAnswer();
-    String returnOperator();
+    int returnOperator();
     double returnResult();
 
 
     class transper implements Calculator{
 
         private String answer;
-
-        private int count = 1;
-
         public transper() throws IOException {
             System.out.println("사칙연산 입력 : ");
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-            this.answer =bf.readLine();
+            answer = bf.readLine().trim();
+            answer = answer.replace(" ","");
         }
 
         @Override
@@ -29,20 +28,12 @@ interface Calculator {
         }
 
         @Override
-        public String returnOperator() {
-            String location = null;
-            int count2 = 0;
+        public int returnOperator() {
+            int location = -1;
             for ( int i = 0; i < answer.length(); i++){
                 Character a = answer.charAt(i);
                 if (a == '+' || a == '-' || a == '*' || a == '/'){
-                    count2 += 1;
-                    if (count2 >= 2){
-                        location = location +"," + String.valueOf(i);
-                        count2 = 1;
-                        count +=1;
-                    }
-                    location = String.valueOf(i);
-
+                    location = i;
                 }
             }
             return location;
@@ -51,26 +42,25 @@ interface Calculator {
 
         @Override
         public double returnResult() {
-            for (int j = 0; j < count; j++){
-                if(count < 2){
-                    int location = Integer.parseInt(returnOperator());
-                    if (location == -1){
-                        throw new IllegalArgumentException("잘못된 연산자 입니다.");
-                    }
-                    double left = Double.parseDouble(answer.substring(0,location).trim());
-                    double right = Double.parseDouble(answer.substring(location+1).trim());
-
-                    switch (answer.charAt(location)){
-                        case '+' :  return left + right;
-                        case '-' : return  left - right;
-                        case '*' : return left * right;
-                        case '/' : return  left / right;
-                        default:
-                            throw new IllegalArgumentException("잘못된 연산자 입니다.");
-                    }
-                }
+            int location = returnOperator();
+            if (location == -1){
+                throw new IllegalArgumentException("잘못된 연산자 입니다.");
             }
-         return 0;
+            if (answer.length() -1 <= location ){
+                throw new IllegalArgumentException("연산자 위치가 잘못되었습니다.");
+            }
+
+            double left = Double.parseDouble(answer.substring(0,location));
+            double right = Double.parseDouble(answer.substring(location+1));
+
+            switch (answer.charAt(location)){
+                case '+' :  return left + right;
+                case '-' : return  left - right;
+                case '*' : return left * right;
+                case '/' : return  left / right;
+                default:
+                    throw new IllegalArgumentException("잘못된 연산자 입니다.");
+            }
         }
     }
 }
@@ -79,7 +69,7 @@ public class Main{
     public static void main(String[] args) throws IOException {
         while (true){
             Calculator calculator = new Calculator.transper();
-            System.out.println("수식 반환 : " + calculator.returnAnswer());
+            System.out.println("수식 반환 :" + calculator.returnAnswer());
             System.out.println("연산자 위치 : " + calculator.returnOperator());
             System.out.println("결과 반환 : " + calculator.returnResult() + "\n");
         }
